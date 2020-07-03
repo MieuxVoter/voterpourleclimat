927224b4927db8aa62d255110c3f63d22ccdc427
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2020-07-03 08:21:06 +0200

    fix(stats): update july 3rd

--- src/constants/stats.js
+++ src/constants/stats.js
@@ -1,4 +1,4 @@
-export const numVotes = 554836
-export const numVoters = 20811
+export const numVotes = 615874
+export const numVoters = 21211
 export const goalVotes = 1000000
 export const goalVoters = 100000
